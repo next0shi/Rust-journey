43b112d959c696c67ab9ab09a59ce05cf90ac895
next0shi <khalidopeyemi22@gmail.com>
2026-07-03 19:05:27 +0100

    Used cargo build

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-6208ca36d21e0aec.d
@@ -0,0 +1,5 @@
+/home/nextx0x/Rust-journey/projects/hello_cargo/target/debug/deps/hello_cargo-6208ca36d21e0aec.d: src/main.rs
+
+/home/nextx0x/Rust-journey/projects/hello_cargo/target/debug/deps/libhello_cargo-6208ca36d21e0aec.rmeta: src/main.rs
+
+src/main.rs:
